e6e745ac6b23e877d3dca81dffd63aa9d72231b3
SeolYu, Lee <seolyu.90@gmail.com>
2020-07-01 02:48:01 +0900

    복습 중

--- project/seolyucompany/instagram/admin.py
+++ project/seolyucompany/instagram/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from django.utils.safestring import mark_safe
+from .models import Post
 
-# Register your models here.
+# 등록법 #1
+# admin.site.register(Post)  #기본 ModelAdmin으로 동작
+
+@admin.register(Post)  # Wrapping
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['id', 'photo_tag', 'message', 'message_length', 'is_public', 'created_at', 'updated_at']
+    list_display_links = ['message']  # 여러개 가능
+    list_filter = ['created_at', 'is_public']
+    search_fields = ['message']
+
+    def photo_tag(self, post):
+        if post.photo:
+            return mark_safe(f'<img src="{post.photo.url}" style="width: 72px;" />')
+        return None
+
+    def message_length(self, post):
+        # return len(post.message)
+        return f"{len(post.message)} 글자"
--- project/seolyucompany/instagram/models.py
+++ project/seolyucompany/instagram/models.py
@@ -2,5 +2,16 @@ from django.db import models
 
 class Post(models.Model):
     message = models.TextField()
+    photo = models.ImageField(blank=True, upload_to='instagram/post/%Y/%m/%d/%H/%M/%S')
+    is_public = models.BooleanField(default=False, verbose_name='공개여부')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        # return f"Custom Post object ({self.id})"
+        # return "Custom Post object ({})".format(self.id)
+        return self.message
+    
+    # def message_length(self):  # 인자없는 함수만 가능
+    #     return len(self.message)
+    # message_length.short_description = "메세지 글자수"  # 이름도 변경 가능
