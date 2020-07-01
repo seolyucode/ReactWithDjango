0d0f7af0e3a73fe886867a3254ef0d9e9def8d12
SeolYu, Lee <seolyu.90@gmail.com>
2020-07-01 20:13:37 +0900

    ES6

--- memo/js/es6.js
+++ memo/js/es6.js
@@ -1,4 +1,6 @@
 const tom = { lang: "Python" };
 tom.lang = "Javascript";
+tom["lang"] = "Ruby";
 
+console.log(tom["lang"]);
 console.log(tom);
---
+++ memo/js/es6공부.js
@@ -0,0 +1,8 @@
+const tom2 = {
+  name: "Tom",
+  age: 10,
+  region: "Seoul",
+  ["score" + "1"]: 100, // Array 아님
+};
+
+console.log(tom2);
